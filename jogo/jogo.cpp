a951f28d5bee88ed2e155bdf763ea7d4bdd9af18
fabiogabriel643 <68522036+fabiogabriel643@users.noreply.github.com>
2025-10-29 17:11:30 -0300

    jogoaleterado com teste de classe

--- jogo/jogo.cpp
+++ jogo/jogo.cpp
@@ -2,3 +2,16 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+
+	class ente {
+		public:
+		int x, y;
+		void mover(int dx, int dy) {
+			x += dx;
+			y += dy;
+		}
+	};
+}
